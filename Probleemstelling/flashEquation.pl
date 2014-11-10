0ee9019be22fe501a8b2c7839106c4e2a023c0bf
CrapsJeroen <Jeroen.Craps@student.kuleuven.be>
2014-11-10 17:06:24 +0100

    Updated flashEquation.pl (-,/)

--- Probleemstelling/flashEquation.pl
+++ Probleemstelling/flashEquation.pl
@@ -1,3 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%
+% Start function
+%%%%%%%%%%%%%%%%%%%%
+
 start(Eq,Answer):- 
 	expression(E1,1), 
 	operand(O), 
@@ -6,10 +10,15 @@ start(Eq,Answer):-
 	append(T,E2,Eq),
 	solveEQ(Eq,Answer).
 
-expression([1],_).
+%%%%%%%%%%%%%%%%%%%%%%
+% Expressies 
+% uitwerkingen
+%%%%%%%%%%%%%%%%%%%%%%
+
+expression([1],_). % Invullen expressie
 expression([2],_).
 expression([3],_).
-expression(Eq,D):- 
+expression(Eq,D):- % E -> E O E
 	D<3,
 	plus(D,1,NewD),
 	expression(E1,NewD), 
@@ -18,11 +27,18 @@ expression(Eq,D):-
 	append(E1,O,T), 
 	append(T,E2,Eq).
 
-
+%%%%%%%%%%%
+% Operands
+%%%%%%%%%%%
 
 operand([+]).
 operand([*]).
-%operand([-]).
+operand([-]).
+operand([/]).
+
+%%%%%%%%%
+% Solve
+%%%%%%%%%
 
 solve([+],[E1],[E2],Answer):- 
 	plus(E1,E2,Answer).
@@ -30,26 +46,30 @@ solve([-],[E1],[E2],Answer):-
 	minus(E1,E2,Answer).
 solve([*],[E1],[E2],Answer):- 
 	multiply(E1,E2,Answer).
+solve([/],[E1],[E2],Answer):- 
+	division(E1,E2,Answer).
+
+%%%%%%%%%%%%%%%%%%%%%%
+% Solve Equation:
+% *1 niet toegelaten
+%%%%%%%%%%%%%%%%%%%%%%
 
 solveEQ([Answer],Answer).	
-solveEQ([_,*,1|_], _):- fail.
+%solveEQ([_,*,1|_], _):- fail.
 solveEQ([E1,O,E2|Rest], Answer):-
-	( 	=(O,*), 
-		((E2 == 1); (E1 == 1))
+	( 	((=(O,*),((E2 == 1); (E1 == 1))) ;(=(O,/),(E2 == 1)))
 		->
 		fail;
-		( E1 > Answer
-			-> 
-			fail;
 		
 	solve([O],[E1],[E2], Result),
 	append([Result],Rest, NewList),
-	solveEQ(NewList, Answer))).
-
+	solveEQ(NewList, Answer)).
 	
+%%%%%%%%%%%%%%%%%%%%%%%
+% BasicFunctions	
+%%%%%%%%%%%%%%%%%%%%%%%
 
-	
-%%BasicFunctions	
 plus(A, B, C) :- C is A + B.
 minus(A, B, C) :- C is A - B.
 multiply(A, B, C) :- C is A * B.
+division(A, B, C) :- C is A / B.
