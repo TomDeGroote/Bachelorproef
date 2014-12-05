e2dfc6aeae1ee0765429e01c212c8b5169856f5c
TomDeGroote <degroote_tom@hotmail.com>
2014-12-05 20:36:05 +0100

    levelCount + Commentaar in TreeMain

--- Programma's/FlashEquation2/src/tree/TreeMain.java
+++ Programma's/FlashEquation2/src/tree/TreeMain.java
@@ -14,14 +14,25 @@ public class TreeMain {
 	private static Equation startEquation;
 	private static List<List<Equation>> listOfLevels;
 	
+	/**
+	 * Main method of the tree-program
+	 * This will initialize the tree, expand it and prune it
+	 * 
+	 * When the tree is build it will be written to a file (as an object)
+	 */
 	public static void main(String[] args) {
 		initialize();
 		for(int i = 0; i < nrOfLevels; i++) {
 			expand();
 			prune();
+			levelCount++;
 		}
+		// TODO write to file
 	}
 	
+	/**
+	 * Creates the first level, and initializes levelCount
+	 */
 	private static void initialize() {
 		levelCount = 1;
 		startEquation = new Equation();
@@ -31,6 +42,10 @@ public class TreeMain {
 		listOfLevels.add(firstLevel);
 	}
 	
+	/**
+	 * Expands the current level (given by levelCount)
+	 * 		After expansion the level will be added to the level list
+	 */
 	private static void expand() {
 		List<Equation> newLevel = new ArrayList<Equation>();
 		for(Equation currentEquation : listOfLevels.get(levelCount)) {
@@ -40,6 +55,10 @@ public class TreeMain {
 		}
 	}
 	
+	/**
+	 * Prunes the current level (given by levelCount)
+	 * 		Because a pointer will be given, the current level will be changed
+	 */
 	private static void prune() {
 		PruneRules.prune(listOfLevels.get(levelCount));
 	}
