03874afbd81b0a402a6f5b480b6345e6708b6137
TomDeGroote <degroote_tom@hotmail.com>
2014-12-07 15:51:27 +0100

    Equation geschreven

--- Programma's/FlashEquation2/src/tree/Equation.java
+++ Programma's/FlashEquation2/src/tree/Equation.java
@@ -11,33 +11,54 @@ public class Equation {
 	List<Symbol> listOfSymbols;
 	String representation;
 	
+	/**
+	 * Constructor when no parameters are given
+	 * This constructor will generate an equation = E
+	 * 
+	 */
 	public Equation(){
 		Symbol startSymbol = new NonTerminal("E");
-		List<Symbol> newList = new ArrayList<Symbol>();
-		newList.add(startSymbol);
-		listOfSymbols = newList;
+		this.listOfSymbols.add(startSymbol);
+		this.setRepresentation();
+	}
+	
+
+	/**
+	 * This constructor will generate a equation containing the symbols
+	 * of inputlist using those symbols in the same order.
+	 * @param inputlist
+	 * 			List of symbols in order for this equation
+	 */
+	public Equation(List<Symbol> inputlist){
+		this.listOfSymbols = inputlist;
+		this.setRepresentation();
+	}
+	
+	/**
+	 * Creates a representation of the current equation represented
+	 * by listOfSymbols. this.representation is then set to the representation created.
+	 */
+	private void setRepresentation() {
 		String currentString = "";
 		for(Symbol currentSymbol: this.getListOfSymbols() ){
 			currentString.concat(currentSymbol.toString());
 		}
 		this.representation = currentString;
 	}
-	
-	public Equation(List<Symbol> inputlist){
-		listOfSymbols = inputlist;
-		String currentString = "";
-		for(Symbol currentSymbol: this.getListOfSymbols() ) {
-			currentString.concat(currentSymbol.toString());
-		}
-		this.representation = currentString;
-	}
-	
+
+	/**
+	 * @return
+	 * 		Returns the list of symbols of this equation, the symbols are in order.
+	 */
 	public List<Symbol> getListOfSymbols() {
 		return this.listOfSymbols;
 	}
 	
+	/**
+	 * Returns a string version of this equation
+	 */
 	@Override
 	public String toString() {
-		return representation;
+		return this.representation;
 	}
 }
