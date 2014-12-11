a6ce762b0a15c2fb7c4761e39a59ef8d5eaad708
CrapsJeroen <Jeroen.Craps@student.kuleuven.be>
2014-12-12 00:00:03 +0100

    Added Input
    Input Class ready for testing

--- Programma's/FlashEquation2/src/master/Input.java
+++ Programma's/FlashEquation2/src/master/Input.java
@@ -1,5 +1,47 @@
 package master;
 
-public class Input {
+import java.io.*;
+import java.util.*;
 
+public class Input {
+	
+	private File text;
+	private List<List<Double>> inputList;
+	
+	public Input() {
+		inputList = new ArrayList<List<Double>>();
+		try {
+			text = new File("src/inputExample.txt");
+			readFile1(text);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void readFile1(File fin) throws IOException {
+		FileInputStream fis = new FileInputStream(fin);
+	 
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+	 
+		String line = null;
+		ArrayList<Double> doublesLine;
+		while ((line = br.readLine()) != null) {
+			String[] test = line.split(" ");
+			doublesLine = new ArrayList<Double>();
+			for(String t : test){
+				doublesLine.add(Double.parseDouble(t));
+			}
+			inputList.add(doublesLine);
+		}
+		
+	 
+		br.close();
+	}
+	
+	public List<List<Double>> getList() {
+		return inputList;
+	}
+	
 }
