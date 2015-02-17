bc9c7189424d3664dab3b5146ccb8270f2d30dc2
TomDeGroote <degroote_tom@hotmail.com>
2015-02-17 17:48:59 +0100

    Research

--- Programma's/FlashEquation2/src/research/Statistics.java
+++ Programma's/FlashEquation2/src/research/Statistics.java
@@ -27,12 +27,6 @@ public class Statistics {
 	private static final boolean printBest = true;
 	private static final boolean printTime = true;
 	private static final boolean printRandom = true;
-	
-	// RandomGenerator parameters
-	private static final int length = 5; // inclusive solution
-	private static final int nrOfExamples = 5;
-	private static final int minimum = -100;
-	private static final int maximum = 100;
 
 	// inputs
 	private static Input inputP = null;
@@ -42,14 +36,46 @@ public class Statistics {
 		boolean realRandom = false;	
 		inputP = new Input(Tree.FILENAME_P);
 		inputNP = new Input(Tree.FILENAME_NP);
-
+		PvsNP();
 	}
 	
 	public static void PvsNP() {
+		String statisticsAll_P = "Time - Size - Best\n";
+		String statisticsTuple_P = "Time - Size - Best\n";
+		String statisticsAll_NP = "Time - Size - Best\n";
+		String statisticsTuple_NP = "Time - Size - Best\n";
+		String randomUsed = "";
 		
+		Master objectMaster = new ObjectMasterAllSolutions();
+		Master tupleMaster = new ObjectMasterAllSolutions();
+
+		for(int i = 0; i < 5; i++) {	
+			System.out.println("Experiment: " + (i+1));
+			List<List<Double>> numbers = genetereNumbers(true, 5, 1, 0, 100);
+			statisticsAll_P += runStatistic(Runner.OBJECTALL, inputP, numbers, true, true) + "\n";
+			statisticsTuple_P += runStatistic(Runner.TUPLE, inputP, numbers, true, true) + "\n";
+			statisticsAll_NP += runStatistic(Runner.OBJECTALL, inputNP, numbers, true, true) + "\n";
+			statisticsTuple_NP += runStatistic(Runner.OBJECTALL, inputNP, numbers, true, true) + "\n";
+
+			randomUsed += getRandomString(true, numbers) + "\n\n";
+			System.out.println("Done!");
+		}
+		
+		// TODO write slechts de files van object
+		
+		writeToFile(statisticsAll_P, "PvsNP_P" + "_" + objectMaster.getNameOfMaster());
+		writeToFile(statisticsTuple_P, "PvsNP_P" + "_" + tupleMaster.getNameOfMaster());
+		writeToFile(statisticsAll_NP, "PvsNP_NP" + "_" + objectMaster.getNameOfMaster());
+		writeToFile(statisticsTuple_NP, "PvsNP_NP" + "_" + tupleMaster.getNameOfMaster());
+		writeToFile(randomUsed, "PvsNP_Random");
 	}
 	
-	public static List<List<Double>> genetereNumbers(boolean useRealRandom) {
+	/**
+	 * Generates random numbers
+	 * @param useRealRandom
+	 * @return
+	 */
+	public static List<List<Double>> genetereNumbers(boolean useRealRandom, int length, int nrOfExamples, int minimum, int maximum) {
 		List<List<Double>> numbers = null;
 		if(useRealRandom) {
 			numbers = RandomGenerator.generateCertainAllK(length, nrOfExamples, minimum, maximum); // Real Random
@@ -62,11 +88,9 @@ public class Statistics {
 	}
 	
 	/**
-	 * Runs every Master whose name is in the parameter strings
-	 * @param strings
-	 * 			List containing the names of the to run Masters
+	 * Runs Master whose name is in the parameter Runner
 	 */
-	public static void runStatistic(Runner runner, Input input, List<List<Double>> numbers, String experimentName, boolean useRealRandom) {
+	public static String runStatistic(Runner runner, Input input, List<List<Double>> numbers, boolean useRealRandom, boolean raw) {
 		Master master = null;
 		switch (runner) {
 		case TUPLE:
@@ -86,9 +110,18 @@ public class Statistics {
 			break;
 		}	
 		// run the master
-		runMaster(master, input, DEADLINE, stopAfterOne, numbers, printSizeAll, printAll, printBest, printTime, printRandom, experimentName);
-		
-		
+		return runMaster(master, input, DEADLINE, stopAfterOne, numbers, printSizeAll, printAll, printBest, printTime, printRandom, raw);
+	}
+	
+	
+	/**
+	 * Writes the random used numbers to a file named experimentName_Random
+	 * @param useRealRandom
+	 * 		Was realRandom used or not
+	 * @param numbers
+	 * 		The numbers to be written away
+	 */
+	private static String getRandomString(boolean useRealRandom, List<List<Double>> numbers) {
 		String s = "";
 		if(useRealRandom) {
 			s += "***   To be found: " + RandomGenerator.getLastGeneratedEquation() + "\n";		
@@ -106,7 +139,7 @@ public class Statistics {
 			}
 			s += "\n";
 		}
-		writeToFile(s, experimentName + "_Random");
+		return s;
 	}
 	
 
@@ -121,35 +154,43 @@ public class Statistics {
 	 * @param printAll
 	 * @param printBest
 	 * @param printTime
+	 * @param raw
+	 * 		If raw returns a string : "time  size  best"
 	 */
-	private static void runMaster(Master master, Input input, int deadline, boolean stopAfterOne, List<List<Double>> numbers,boolean printSizeAll, boolean printAll, boolean printBest, boolean printTime, boolean printRandom, String experimentName) {
+	private static String runMaster(Master master, Input input, int deadline, boolean stopAfterOne, List<List<Double>> numbers,boolean printSizeAll, boolean printAll, boolean printBest, boolean printTime, boolean printRandom, boolean raw) {
 		String toPrint = "";
-		toPrint += "***   Executing " + master.getNameOfMaster() + "\n";
+		if(!raw) {
+			toPrint += "***   Executing " + master.getNameOfMaster() + "\n";
+		}
 		// run master
 		long time = System.currentTimeMillis();
 		master.run(deadline, stopAfterOne, numbers, input);
 		time = System.currentTimeMillis() - time;
 		
-		if(printTime) {
-			toPrint +=  "  -     Time: " + time + "\n";
-		}
-		if(printSizeAll) {
-			toPrint += "  -     Size: " + master.getAllSolutions().size() + "\n"; 
-		}
-		if(printBest) {
-			try {
-				toPrint += "  -     Best: " + master.getBestSolution().toString() + "\n";
-			} catch (NullPointerException e) {
-				toPrint += "  -     Best: Empty" + "\n";
+		if(!raw) {
+			if(printTime) {
+				toPrint +=  "  -     Time: " + time + "\n";
 			}
-		}
-		if(printAll) {
-			toPrint += "  -     All:\n";
-			for(Equation eq : master.getAllSolutions()) {
-				toPrint += "          + " + eq.toString() + "\n";
+			if(printSizeAll) {
+				toPrint += "  -     Size: " + master.getAllSolutions().size() + "\n"; 
 			}
+			if(printBest) {
+				try {
+					toPrint += "  -     Best: " + master.getBestSolution().toString() + "\n";
+				} catch (NullPointerException e) {
+					toPrint += "  -     Best: Empty" + "\n";
+				}
+			}
+			if(printAll) {
+				toPrint += "  -     All:\n";
+				for(Equation eq : master.getAllSolutions()) {
+					toPrint += "          + " + eq.toString() + "\n";
+				}
+			}
+		} else {
+			toPrint += "" + time + "  " + master.getAllSolutions().size() + "  " + master.getBestSolution();
 		}
-		writeToFile(toPrint, experimentName + "_" + master.getNameOfMaster());
+		return toPrint;
 	}
 	
 	/**
