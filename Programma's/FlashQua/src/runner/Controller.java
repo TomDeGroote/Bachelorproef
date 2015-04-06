ca421d0edd5a36065623dc58e3c50ba99744646a
TomDeGroote <degroote_tom@hotmail.com>
2015-04-06 17:29:08 +0200

    Last commit before the world ends

---
+++ Programma's/FlashQua/src/runner/Controller.java
@@ -0,0 +1,118 @@
+package runner;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import exceptions.MaxLevelReachedException;
+import exceptions.OutOfTimeException;
+import treebuilder.Equation;
+import treebuilder.comparators.Comparator;
+import treebuilder.grammar.Grammar;
+import treebuilder.tree.MultithreadedTree;
+import treebuilder.tree.Tree;
+
+public class Controller {
+
+	private static Equation bestEquation;
+	private static int nrOfSolutions;
+	private static HashSet<Equation> allEquations;
+	
+	// Deadline and maxlevel parameters
+	private final static int DEADLINE = 2000;
+	private final static int MAXLEVEL = -1;
+	
+	// the weights to be used (basically constants)
+	private final static double[] WEIGHTS = new double[]{1.0, 2.0, 3.0, 5.0, 7.0};	
+
+	private static Tree tree;
+	
+	public void excecute(List<double[]> input, Object comparator) {		
+		Comparator comp = convertToComparator(comparator);
+		List<Comparator> comparators = new ArrayList<>();
+		comparators.add(comp);
+		Grammar.setColumnValues(input, WEIGHTS, comparators);
+		expandTree();
+		allEquations = Grammar.getSolutions();
+		nrOfSolutions = allEquations.size();
+		bestEquation = getBestEquation(allEquations);
+	}
+
+	/**
+	 * Converts an object (String to a comparator)
+	 * @param comparator
+	 * 			The object to convert
+	 * @return
+	 * 			The comparator
+	 */
+	private Comparator convertToComparator(Object comparator) {
+		return Comparator.parseComparator((String) comparator);
+	}
+
+	/**
+	 * Expands the tree
+	 */
+	private void expandTree() {
+		long start = System.currentTimeMillis();
+		if(tree == null) {
+			tree = new MultithreadedTree();
+		}
+		try {
+			tree.expand(start, DEADLINE, MAXLEVEL);
+		} catch(OutOfTimeException e) {
+			System.out.println(e.getMessage());
+		} catch(MaxLevelReachedException e) {
+			System.out.println(e.getMessage());
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Done: " + (System.currentTimeMillis()-start));
+	}
+	
+	/**
+	 * @return the best equation
+	 */
+	public Equation getBestEquation() {
+		return bestEquation;
+	}
+	
+	/**
+	 * @return number of equations
+	 */
+	public int getNrOfSolutions() {
+		return nrOfSolutions;
+	}
+	
+	/**
+	 * @return all equations
+	 */
+	public HashSet<Equation> getAllEquations() {
+		return allEquations;
+	}
+	
+	/**
+	 * @param numbers
+	 * 			The numbers to use in the best equation
+	 * @return the result of the equation if these numbers are filled in
+	 */
+	public double calculateAccordingToBestEquation(double[] numbers) {
+		return Grammar.calculateValue(getBestEquation(), numbers);
+	}
+	
+	/**
+	 * @param allEquations
+	 * 			The equations in which to search for the best equation
+	 * @return The best equation
+	 */
+	private Equation getBestEquation(HashSet<Equation> allEquations) {
+		Equation best = (Equation) allEquations.toArray()[0];
+		for(Equation eq : allEquations) {
+			if(best.getEquationQuality() < eq.getEquationQuality()) {
+				best = eq;
+			} else if(best.getEquationQuality() == eq.getEquationQuality() && best.size() > eq.size()) {
+				best = eq;
+			}
+		}
+		return best;
+	}
+}
--- Programma's/FlashQua/src/treebuilder/comparators/Comparator.java
+++ Programma's/FlashQua/src/treebuilder/comparators/Comparator.java
@@ -14,4 +14,28 @@ public abstract class Comparator {
 	public abstract boolean compareOK(double value1, double value2);
 	
 	public abstract String getRepresentation();
+	
+	/**
+	 * Parses the comparator to be used, default -> =
+	 * @param comparator
+	 * 			The comparator to be parsed
+	 * @return
+	 * 			The Comparator (as a class)
+	 */
+	public static Comparator parseComparator(String comparator) {
+		switch (comparator) {
+		case "=":
+			return new Equals();
+		case "<":
+			return new SmallerThan();
+		case "<=":
+			return new SmallerThanOrEquals();
+		case ">":
+			return new GreaterThan();
+		case ">=":
+			return new GreaterThanOrEquals();
+		default:
+			return new Equals();
+		}
+	}
 }
